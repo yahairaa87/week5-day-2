9b67baac41499f4725dac9eeb43214bce1280ed9
yahaira87 <109757636+yahairaa87@users.noreply.github.com>
2022-09-21 17:05:02 +0000

    sets + adding and removing objects

--- main.py
+++ main.py
@@ -49,25 +49,35 @@ a, b, c, d = my_tuple
 print(a,b,c,d)
 
 #################################################sets######################################################
+# sets are a mutal data structure for storing information whose elements do not repeat
+#two ways of making sets
+set1 = set((1,1,1,2,3,4))
+print(set1)
 
-
+set2 = {1,2,3,4,4,4,4,4,4,4,5}
+print(set2)
+print(7 in set1)
 # Join the following sets into one, called my_set_3:
 
-{1, 2, "three", "four"}
+set0 = {1, 2, "three", "four"}
 
-{"three", 4, 5}
+set01 = {"three", 4, 5}
+set3 = set0.union(set01)
+print(set3)
 
 
 # Remove a random item from the following set, using set methods.
 
 raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
 
-
+raffle.remove("Phoebe")
+print(raffle)
 
 # Add the name Gunther to the following set, using set methods:
 
-raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-
+raffle1 = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+raffle1.add("Gunther")
+print(raffle1)
 #################################################booleans######################################################
 # booleans
 # a boolean can only have 2 values -- true or False
